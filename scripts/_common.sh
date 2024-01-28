041a019a8e12d3d965b4625fdf1be02a509eab8a
hazzamyman <119055745+hazzamyman@users.noreply.github.com>
2024-01-28 20:22:27 +1000

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,33 @@
 #=================================================
 # PERSONAL HELPERS
 #=================================================
+_download_nzbget_from_docker() {
+    docker_image="linuxserver/nzbget"
+    debian=$(lsb_release --codename --short)
+    if [[ $debian = "bullseye" ]]; then
+        docker_version="$(ynh_app_upstream_version)"
+    elif [[ $debian = "bookworm" ]]; then
+        docker_version="$(ynh_app_upstream_version)-alpine"
+    fi
 
+    docker_arg=""
+    # Fixup for armhf
+    if [ "$YNH_ARCH" == "armhf" ]; then
+        docker_arg="--os_arch_variant=linux/arm/v7"
+    fi
+
+    ynh_docker_image_extract --dest_dir="$install_dir/build/" --image_spec="$docker_image:$docker_version" $docker_arg
+
+    # Move files from the extract to the live directory
+    ynh_secure_remove --file="$install_dir/live/"
+    mkdir -p "$install_dir/live/"
+    mv -f "$install_dir/build/"{nzbget} "$install_dir/live/"
+    ynh_secure_remove --file="$install_dir/build"
+
+    chmod 750 "$install_dir"
+    chmod -R o-rwx "$install_dir"
+    chown -R $app:$app "$install_dir"
+}
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
