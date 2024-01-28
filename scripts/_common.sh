8d95c0da2b3a2af3d45a9c974ed8b094d386e15f
hazzamyman <119055745+hazzamyman@users.noreply.github.com>
2024-01-28 21:08:38 +1000

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,17 +8,9 @@
 # PERSONAL HELPERS
 #=================================================
 _download_nzbget_from_docker() {
-    docker_image="linuxserver/nzbget"
 
-
-    docker_arg=""
-    # Fixup for armhf
-    if [ "$YNH_ARCH" == "armhf" ]; then
-        docker_arg="--os_arch_variant=linux/arm/v7"
-    fi
-
-    ynh_docker_image_extract --dest_dir="$install_dir/build/" --image_spec="$docker_image:$docker_version" $docker_arg
-
+ynh_docker_image_extract --dest_dir="$install_dir" --image_spec="nzbget:latest"
+]
     # Move files from the extract to the live directory
     ynh_secure_remove --file="$install_dir/live/"
     mkdir -p "$install_dir/live/"
